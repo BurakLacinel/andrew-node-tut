1b9a2c19a24850d1f6d03c36fd0ee832d71fa2eb
buraklacinel <buraklacinel@gmail.com>
2021-12-02 20:12:40 +0300

    intro to import files 2

--- 1-Module System/app.js
+++ 1-Module System/app.js
@@ -1,4 +1,3 @@
-require("./utils.js");
+const name = require("./utils.js");
 
-// "require" edilmesine rağmen, "utils.js" içerisindeki değişkenlere buradan ulaşamayız.
 console.log(name);
--- 1-Module System/utils.js
+++ 1-Module System/utils.js
@@ -1,1 +1,6 @@
+console.log("utils.js");
+
 const name = "Burak";
+
+// "name"i export ettiğimiz için, "utils"in "require" edildiği dosyalarda kullanılabilir.
+module.exports = name;
