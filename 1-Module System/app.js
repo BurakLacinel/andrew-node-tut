47b3082f3f4c735384c12ae76e829573f6355608
buraklacinel <buraklacinel@gmail.com>
2021-12-03 19:28:23 +0300

    command line argument

--- 1-Module System/app.js
+++ 1-Module System/app.js
@@ -2,7 +2,5 @@ const chalk = require("chalk");
 
 const notes = require("./notes.js");
 
-const msg = notes();
-console.log(msg);
-
-console.log(chalk.green.bold.inverse("Success"));
+// terminal'de dosyayı çalıştırmak için "node app" yerine, "node app.js burak" komudunu girersek; aşağıdaki kod'dan "burak" argument'ı döner.
+console.log(process.argv[2]);
