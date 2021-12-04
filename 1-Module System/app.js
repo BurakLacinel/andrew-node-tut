34ecc726354437e8c8765f30446ce69a815487f2
buraklacinel <buraklacinel@gmail.com>
2021-12-04 14:00:48 +0300

    Command Line Arguments

--- 1-Module System/app.js
+++ 1-Module System/app.js
@@ -1,6 +1,15 @@
 const chalk = require("chalk");
-
 const notes = require("./notes.js");
 
-// terminal'de dosyayı çalıştırmak için "node app" yerine, "node app.js burak" komudunu girersek; aşağıdaki kod'dan "burak" argument'ı döner.
-console.log(process.argv[2]);
+const command = process.argv[2];
+
+if (command === "add") {
+  console.log("Adding Note!");
+} else if (command === "remove") {
+  console.log("Removing note!");
+}
+
+// "node app.js add --title="This is my title"" komuduyla "argument" olarak "title" ekleyebiliriz.
+// eklediğimiz "argument"ın 3.index te döndüğünü görürüz.
+// "parsed"(çözümlenmeden) olmadan döner. String'i parse'lamak için npm ile "library" yükleyeceğiz.
+console.log(process.argv);
